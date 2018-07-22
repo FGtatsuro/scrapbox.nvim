9c816b7799ef069bb1cc9884e491ade758d28160
FGtatsuro <fujiistorage@gmail.com>
2018-07-22 17:52:03 +0900

    Add a command 'ScrapboxAccessPage' to open a scrapbox page with name.

--- rplugin/python3/scrapbox/__init__.py
+++ rplugin/python3/scrapbox/__init__.py
@@ -3,16 +3,24 @@
 
 import neovim
 
+from scrapbox.scrapbox import ScrapboxHandler
+
 
 @neovim.plugin
 class ScrapboxPlugin():
 
     def __init__(self, nvim):
         self.nvim = nvim
+        self.handler = ScrapboxHandler()
 
     @neovim.command('ScrapboxTestCommand')
     def test_command(self):
         self.nvim.command('echo "TestCommand"')
+
+    @neovim.command('ScrapboxAccessPage', nargs=1)
+    def access_page(self, args):
+        title, = args
+        self.handler.access_page(title)
 
 # https://wiki.python.org/moin/Vim
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
