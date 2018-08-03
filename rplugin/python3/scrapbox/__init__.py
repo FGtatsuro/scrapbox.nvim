bb0ef14b08ced6a76e57863996c6b965afd2bd0e
FGtatsuro <fujiistorage@gmail.com>
2018-08-03 09:06:25 +0900

    Adjust range origin.

--- rplugin/python3/scrapbox/__init__.py
+++ rplugin/python3/scrapbox/__init__.py
@@ -17,7 +17,9 @@ class ScrapboxPlugin():
     @neovim.command('ScrapboxAccessPage', nargs='1', range='%')
     def access_page(self, args, range):
         title = args[0]
-        body = '\n'.join(self.nvim.current.buffer[slice(*range)])
+        start, end = range
+        # range starts 1, but Neovim API to get lines accept 0 origin.
+        body = '\n'.join(self.nvim.current.buffer[slice(start - 1, end)])
         self.handler.access_page(title, body)
 
 # https://wiki.python.org/moin/Vim
